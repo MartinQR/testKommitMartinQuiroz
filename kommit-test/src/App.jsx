734c1f2fdf991670b62ec2754805fb3237c727a8
MartinQR <quirozrmartin@gmail.com>
2025-10-06 09:08:54 -0600

    feature(komit): crear prueba tecnica de kommit martin quiroz

---
+++ kommit-test/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import BodyList from "./components/bodyList";
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <BodyList />
+    </div>
+  );
+}
+
+export default App;
